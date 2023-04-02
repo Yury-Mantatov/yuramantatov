48035a2baa1083ebbc68649109f1d0d8c23a4b4c
Yury-Mantatov <127655046+Yury-Mantatov@users.noreply.github.com>
2023-04-02 23:36:46 +0700

    Create Handler2.cs

---
+++ Handler2.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Handler2
+    {
+        public void Message()
+        {
+            Console.WriteLine("Уже 77, давно пора было начать");
+        }
+    }
+}
